ff6e478bd94ee2b46a193e44ae07ba8a115dddab
Kevin Du <kdu4108@users.noreply.github.com>
2017-12-15 16:42:52 -0500

    frame_stack enjoy

--- baselines/deepq/experiments/atari/enjoy.py
+++ baselines/deepq/experiments/atari/enjoy.py
@@ -12,7 +12,8 @@ from baselines.common.misc_util import (
     boolean_flag,
 )
 from baselines import bench
-from baselines.common.atari_wrappers import wrap_deepmind
+from baselines.common.atari_wrappers import wrap_
+
 from baselines.deepq.experiments.atari.model import model, dueling_model
 
 
@@ -31,7 +32,7 @@ def parse_args():
 def make_env(game_name):
     env = gym.make(game_name + "NoFrameskip-v4")
     env = bench.Monitor(env, None)
-    env = wrap_deepmind(env)
+    env = wrap_deepmind(env, frame_stack=True, scale=True)
     return env
 
 
