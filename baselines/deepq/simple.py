98a044a9adc73db4931dd4877131f59bb2e75797
Kevin Du <kevin_du@brown.edu>
2018-02-06 00:35:39 -0500

    3.4 for 1f3c

--- baselines/deepq/simple.py
+++ baselines/deepq/simple.py
@@ -5,11 +5,13 @@ import tempfile
 import tensorflow as tf
 import zipfile
 
+import baselines
 import baselines.common.tf_util as U
 
 from baselines import logger
 from baselines.common.schedules import LinearSchedule
-from baselines import deepq
+#from baselines import deepq
+from baselines.deepq.build_graph import build_act, build_train
 from baselines.deepq.replay_buffer import ReplayBuffer, PrioritizedReplayBuffer
 
 
@@ -169,7 +171,7 @@ def learn(env,
     def make_obs_ph(name):
         return U.BatchInput(env.observation_space.shape, name=name)
 
-    act, train, update_target, debug = deepq.build_train(
+    act, train, update_target, debug = baselines.deepq.build_graph.build_train(
         make_obs_ph=make_obs_ph,
         q_func=q_func,
         num_actions=env.action_space.n,
